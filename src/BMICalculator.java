a4b5924a84a1170dfe00242944556b84920d47f2
23lukes <23lukes@da.org>
2022-11-14 10:52:07 -0500

    Initial commit

---
+++ src/BMICalculator.java
@@ -0,0 +1,9 @@
+public class BMICalculator {
+    /** Convert English to metric units, perform the BMI calculation
+     * NOTE:
+     *
+     */
+    public static double computeBMI(int inches, int pounds) {
+
+    }
+}
